3ca8523bf8a28063f818183f3c216255d0439e85
Kalle Janssen <Kallejanssen@me.com>
2018-06-05 16:40:38 +0200

    You can now choose different years in python will improve soon I think

--- bokehtest.py
+++ bokehtest.py
@@ -4,13 +4,20 @@ from numpy import histogram
 import pandas as pd
 import numpy as np
 from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models.widgets import CheckboxGroup
 
+year = input("Which year do you want to look at? ")
 
-uniranking = pd.read_csv('university_ranking.csv')
+uniranking1 = pd.read_csv('university_ranking.csv')
+if year == 'all':
+    uniranking = uniranking1
+elif '2016' or '2017' or '2018':
+    uniranking = uniranking1.loc[uniranking1['year'] == int(year)]
+else:
+    year = input("Please choose 2016, 2017, 2018 or all.")
+
 p = figure(plot_width = 600, plot_height = 600, title ='University grade and ranking correlation',
 x_axis_label ='Overall grade', y_axis_label = 'Amount of universities')
-#p.figure(x=df.ranking, y=df.score_overall)
-print(uniranking['score_overall'].describe())
 
 # Makes a histogram from range 0 to 100 in steps of 2.
 arr_hist, edges = np.histogram(uniranking['score_overall'],
@@ -35,6 +42,7 @@ p.quad(source = src, bottom=0, top='score_overall',
 hover = HoverTool(tooltips = [('Score', '@left - @right'),
                              ('Amount of universities', '@score_overall')])
 
+#year_selection = CheckboxGroup(labels=available_years, active = [0,1])
 # Add the hover tool to the graph
 p.add_tools(hover)
 
