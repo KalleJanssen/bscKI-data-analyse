c02e3310883f7833b6c166dc3fecf53448574d61
Daniel Perez Jensen <danielperezjensen@outlook.com>
2018-06-15 22:16:08 +0200

    radar plot for every year with highest and lowerst ranked universities

--- radar_plot.py
+++ radar_plot.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource, LabelSet
-from bokeh.layouts import column
+from bokeh.layouts import column, gridplot
 from bokeh.io import show, output_file
-
 
 ## globals ##
 
@@ -38,81 +37,82 @@ verts = unit_poly_verts(theta, centre)
 x = [v[0] for v in verts] 
 y = [v[1] for v in verts] 
 
-def top_5_radar():
-	
-	p = figure(title="Radar plot of 5 highest ranked universities")
+
+def radar_plot(df, year):
+
+	p = figure(title="Radar plot of 5 highest ranked universities in " + str(year), x_range=(0, 1.24))
+	t = figure(title="Radar plot of 5 lowest ranked universities in " + str(year), x_range=(0, 1.24))
+
 	text = ['No. of Students per staff member', 'Male', '% Int. Students', 'No. of Students', '']
 	source = ColumnDataSource({'x':x + [centre ],'y':y + [1],'text':text})
-
+	
 	p.line(x="x", y="y", source=source)
+	t.line(x="x", y="y", source=source)
 
-	labels = LabelSet(x="x",y="y",text="text",source=source)
+	p_labels = LabelSet(x="x",y="y",text="text",source=source)
+	t_labels = LabelSet(x="x",y="y",text="text",source=source)
 
-	p.add_layout(labels)
+	p.add_layout(p_labels)
+	t.add_layout(t_labels)
 
-	years = [2016, 2017, 2018]
 	coi = ['no_student_p_staff', 'male', 'pct_intl_student', 'no_student']
-	df = pd.read_csv('university_ranking.csv', index_col=0)
+	df = df[coi]
 
 	# normalizes no_students_p_staff to a value between 0 and 1
 	df['no_student_p_staff'] = normalize(df, 'no_student_p_staff')
 	df['no_student'] = normalize(df, 'no_student')
+
 	# normalizes data to be between 0 and 1
 	df['male'] = df['male'] / 100
-	# gets top 5 universities of given year
-	df = df.loc[df['year'] == 2016].head(5)
-	# gets columns we care about
-	df = df[coi]
+	df_head = df.head(5)
+	df_tail = df.tail(5)
 
-	flist = np.array(df.values.tolist())
-	colors = ['blue','green','red', 'orange','purple']
-	for i in range(len(flist)):
-	    xt, yt = radar_patch(flist[i], theta, centre)
-	    p.patch(x=xt, y=yt, fill_alpha=0.15, fill_color=colors[i])
-	return p
-
-def tail_5_radar():
+	flist_head = np.array(df_head.values.tolist())
+	flist_tail = np.array(df_tail.values.tolist())
 	
-	p = figure(title="Radar plot of 5 lowest ranked universities")
-	text = ['No. of Students per staff member', 'Male', '% Int. Students', 'No. of Students', '']
-	source = ColumnDataSource({'x':x + [centre ],'y':y + [1],'text':text})
-
-	p.line(x="x", y="y", source=source)
-
-	labels = LabelSet(x="x",y="y",text="text",source=source)
-
-	p.add_layout(labels)
-
-	years = [2016, 2017, 2018]
-	coi = ['no_student_p_staff', 'male', 'pct_intl_student', 'no_student']
-	df = pd.read_csv('university_ranking.csv', index_col=0)
-
-	# normalizes no_students_p_staff to a value between 0 and 1
-	df['no_student_p_staff'] = normalize(df, 'no_student_p_staff')
-	df['no_student'] = normalize(df, 'no_student')
-	# normalizes data to be between 0 and 1
-	df['male'] = df['male'] / 100
-	# gets top 5 universities of given year
-	df = df.loc[df['year'] == 2016].tail(5)
-	# gets columns we care about
-	df = df[coi]
-
-	flist = np.array(df.values.tolist())
 	colors = ['blue','green','red', 'orange','purple']
-	for i in range(len(flist)):
-	    xt, yt = radar_patch(flist[i], theta, centre)
-	    p.patch(x=xt, y=yt, fill_alpha=0.15, fill_color=colors[i])
-	return p
 
+	for i in range(len(flist_head)):
+	    xt, yt = radar_patch(flist_head[i], theta, centre)
+	    p.patch(x=xt, y=yt, fill_alpha=0.15, fill_color=colors[i])
+
+	for i in range(len(flist_tail)):
+	    xt, yt = radar_patch(flist_tail[i], theta, centre)
+	    t.patch(x=xt, y=yt, fill_alpha=0.15, fill_color=colors[i])
+
+	return p, t
 
 def main():
 
 	output_file('docs/radar_plot.html')
 
-	p1 = top_5_radar()
-	p2 = tail_5_radar()
+	df = pd.read_csv('university_ranking.csv')
 
-	show(column(p1, p2))
+	years = [2016, 2017, 2018]
+
+	ps = []
+
+	ts = []
+
+	for year in years:
+
+		new_df = df.loc[df['year'] == year]
+
+		p, t = radar_plot(df, year)
+
+		ps.append(p)
+		ts.append(t)
+
+	grid_list = []
+
+	for i in range(len(ps)):
+
+		grid_list.append([ps[i], ts[i]])
+
+	grid = gridplot(grid_list)
+
+	show(grid)
+
 
 if __name__ == "__main__":
 	main()
