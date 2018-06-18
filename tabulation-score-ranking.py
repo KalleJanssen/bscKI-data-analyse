7995af0ac7f6027eb9ab1d9034703e4a629dfc9c
Daniel Perez Jensen <danielperezjensen@outlook.com>
2018-06-18 21:54:02 +0200

    bar chart per region per year

--- tabulation-score-ranking.py
+++ tabulation-score-ranking.py
@@ -22,58 +22,63 @@ def mean_sd(list):
 
 	return mean, sd
 
-df = pd.read_csv('university_ranking.csv', index_col=0)
+def main():
 
-continents = ['Europe', 'America', 'Asia', 'Oceania', 'Africa']      
+	df = pd.read_csv('university_ranking.csv', index_col=0)
 
-df2016 = df.loc[df['year'] == 2016]
-df2017 = df.loc[df['year'] == 2017]
-df2018 = df.loc[df['year'] == 2018]
+	continents = ['Europe', 'America', 'Asia', 'Oceania', 'Africa']      
 
-# all continents put into lists
-continents_2016 = df2016['continents'].tolist()
-continents_2017 = df2017['continents'].tolist()
-continents_2018 = df2018['continents'].tolist()
+	df2016 = df.loc[df['year'] == 2016]
+	df2017 = df.loc[df['year'] == 2017]
+	df2018 = df.loc[df['year'] == 2018]
 
-# all continents counter and put into dictionaries
-count_2016 = dict(Counter(continents_2016))
-count_2017 = dict(Counter(continents_2017))
-count_2018 = dict(Counter(continents_2018))
+	# all continents put into lists
+	continents_2016 = df2016['continents'].tolist()
+	continents_2017 = df2017['continents'].tolist()
+	continents_2018 = df2018['continents'].tolist()
 
-# lists of counts in corrrect order as outlined above
-list2016 = [count_2016[key] for key in continents]
-list2017 = [count_2017[key] for key in continents]
-list2018 = [count_2018[key] for key in continents]
+	# all continents counter and put into dictionaries
+	count_2016 = dict(Counter(continents_2016))
+	count_2017 = dict(Counter(continents_2017))
+	count_2018 = dict(Counter(continents_2018))
 
-data = {}
+	# lists of counts in corrrect order as outlined above
+	list2016 = [count_2016[key] for key in continents]
+	list2017 = [count_2017[key] for key in continents]
+	list2018 = [count_2018[key] for key in continents]
 
-# append required data for mean and sd to dictionary
-for i, continent in enumerate(continents):
-	data[continent] = [list2016[i], list2017[i], list2018[i]]
+	data = {}
 
-means = []
-sds = []
+	# append required data for mean and sd to dictionary
+	for i, continent in enumerate(continents):
+		data[continent] = [list2016[i], list2017[i], list2018[i]]
 
-# calculates mean and sd between years for every given continent
-for country in data:
-	mean, sd = mean_sd(data[country])
-	mean = round(mean, 2)
-	sd = round(sd, 2)
-	means.append(mean)
-	sds.append(sd)
+	means = []
+	sds = []
 
-mean_ranking = []
-sd_ranking = []
-		
+	# calculates mean and sd between years for every given continent
+	for country in data:
+		mean, sd = mean_sd(data[country])
+		mean = round(mean, 2)
+		sd = round(sd, 2)
+		means.append(mean)
+		sds.append(sd)
 
-trace = go.Table(
-		header=dict(values=['Continent', '2016', '2017', '2018', 'mean', 'sd'],
-		line = dict(color='#000000'),
-		fill = dict(color='#B5DBE8')),
-		cells=dict(values=[continents, list2016, list2017, list2018, means, sds],
-			fill = dict(color='#EDEDED')))
+	mean_ranking = []
+	sd_ranking = []
+			
 
-layout = dict(width=500, height=500)
-data = [trace]
-fig = dict(data=data, layout=layout)
-plotly.offline.plot(fig, filename = 'docs/styled_table.html')
+	trace = go.Table(
+			header=dict(values=['Continent', '2016', '2017', '2018', 'mean', 'sd'],
+			line = dict(color='#000000'),
+			fill = dict(color='#B5DBE8')),
+			cells=dict(values=[continents, list2016, list2017, list2018, means, sds],
+				fill = dict(color='#EDEDED')))
+
+	layout = dict(width=500, height=500)
+	data = [trace]
+	fig = dict(data=data, layout=layout)
+	plotly.offline.plot(fig, filename = 'docs/styled_table.html')
+
+if __name__ == "__main__":
+	main()  
