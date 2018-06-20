c53bbb8fd4ef16e0e92b5efcaeedb84df1efc7f4
Daniel Perez Jensen <danielperezjensen@outlook.com>
2018-06-20 02:05:16 +0200

    initial functions for scatterplot

--- bokeh-server/main.py
+++ bokeh-server/main.py
@@ -19,19 +19,20 @@ def drop_change(atrrname, old, new):
 def update_bar_chart():
 
     data = get_data_bar_chart(selection.value)
-    source.data = source.from_df(data[['region', 'list2016', 'list2017', 
-                                       'list2018']])
+    column_source.data = column_source.from_df(data[['region', 'list2016', 'list2017',
+                                                     'list2018']])
 
     # updates table
     update_table(data)
 
     # updates plot title and x_range
-    column_bar_split.title.text = 'Count of universities per region in %s' % (continent)
+    column_bar_split.title.text = 'Count of universities per region in %s' % (selection.value)
     column_bar_split.x_range.factors = data['region'].tolist()
 
 
 def update_table(data):
 
+    data.columns = ['region', '2016', '2017', '2018', 'mean', 'std']
     table.text = str(data[['region', '2016', '2017', '2018', 'mean', 'std']])
 
 
@@ -69,42 +70,64 @@ def get_data_bar_chart(continent):
     return data
 
 
+def get_data_correlation(variable):
+
+    pass
+
+
+def correlation_change(atrrname, old, new):
+
+    update_correlation()
+
+
+def update_correlation():
+
+    data = get_data_correlation(correlation_select.value)
+    print(data)
+
+
 # set up widgets
 table = PreText(text='', width=500)
 static_table = PreText(text='', width=500)
 selection = Select(value='America', options=['America', 'Europe',
                                              'Asia', 'Oceania', 'Africa'])
-
-# set up hover
-hover = HoverTool(
-            tooltips=[('No. of Universities', '@list2016')])
+correlation_select = Select(value='pct_intl_student',
+                            options=['pct_intl_student', 'male',
+                                     'no_student', 'no_student_p_staff'])
 
 # set up plots
-source = ColumnDataSource(data=dict(region=[], list2016=[],
-                                    list2017=[], list2018=[]))
+# Column Bar Plot
+column_source = ColumnDataSource(data=dict(region=[], list2016=[],
+                                 list2017=[], list2018=[]))
 column_bar_split = figure(y_range=(0, 400), x_range=[],
                           plot_height=250, plot_width=700, tools=[])
 column_bar_split.vbar(x=dodge('region', -0.25, range=column_bar_split.x_range),
-                      top='list2016', width=0.2, source=source,
+                      top='list2016', width=0.2, source=column_source,
                       color="#c9d9d3", legend=value("2016"))
 column_bar_split.vbar(x=dodge('region',  0.0,  range=column_bar_split.x_range),
-                      top='list2017', width=0.2, source=source,
+                      top='list2017', width=0.2, source=column_source,
                       color="#718dbf", legend=value("2017"))
 column_bar_split.vbar(x=dodge('region',  0.25, range=column_bar_split.x_range),
-                      top='list2018', width=0.2, source=source,
+                      top='list2018', width=0.2, source=column_source,
                       color="#e84d60", legend=value("2018"))
 column_bar_split.legend.location = 'top_right'
 column_bar_split.legend.orientation = 'horizontal'
 
+# Correlation Between Ranking and Selected Value
+correlation_source = ColumnDataSource(data=dict(ranking=[], variable=[],
+                                      years=[], color=[], university=[]))
+correlation = figure()
+
 # on change switch values
 selection.on_change('value', drop_change)
+correlation_select.on_change('value', correlation_change)
 
 # gets static plot and table string from helper.py
 static_col, static_table_data = helper.bar_chart_continent_split()
 static_table.text = str(static_table_data)
 
 # lays out the widgets and columns
-widgets = column(selection, table)
+widgets = column(selection, table, correlation_select)
 main_row = row(column_bar_split, widgets)
 secondary_row = row(static_col, static_table)
 layout = column(main_row, secondary_row)
