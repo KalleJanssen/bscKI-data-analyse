075c21b30ca0af6751ed0d88475a689e49064cea
Daniel Jensen <danielperezjensen@outlook.com>
2018-06-28 16:51:14 +0200

    fill.py properly commented

--- preprocessing/fill.py
+++ preprocessing/fill.py
@@ -5,10 +5,13 @@ from sklearn.model_selection import train_test_split
 
 def male_female_fill(df):
 
-    # columns of interest for predicting male female split
+    # converts all missing datapoints in column male into
+    # mean of same country
     df.loc[df.male.isnull(), 'male'] = (df.groupby('country')
                                         .male.transform('mean'))
+    # converts type to integer
     df.male = df.male.astype(int)
+    # calculates female percentage based on male percentage
     df['female'] = 100 - df['male']
 
     return df
@@ -16,8 +19,10 @@ def male_female_fill(df):
 
 def pct_intl_student_fill(df):
 
+    # converts string representation to float representation
     df['pct_intl_student'] = (df['pct_intl_student'].str.replace(r'%', r'.0')
                               .astype('float') / 100.0)
+    # fills missing data points with 0 value
     df['pct_intl_student'] = df['pct_intl_student'].fillna(0)
 
     return df
@@ -25,6 +30,7 @@ def pct_intl_student_fill(df):
 
 def score_industry_fill(df):
 
+    # columns we will use for predicting score_industry
     coi = ['ranking',
            'score_overall',
            'score_teaching',
@@ -33,19 +39,28 @@ def score_industry_fill(df):
            'score_int_outlook',
            ]
 
+    # get all rows that do not miss any value in any column
     not_nans = df['score_industry'].notnull()
     df_notnans = df[not_nans]
 
+    # features
     x = np.array(df_notnans[coi])
+    # labels
     y = np.array(df_notnans['score_industry'])
 
+    # splits rows into testing and training data
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
+    # classifier that we use, Multiple Linear Regression
     clf = LinearRegression()
+    # trains classifier on trainig data
     clf.fit(x_train, y_train)
 
+    # prints amount of predicted values that are correct
+    # compares with the testing data
     print("score_industry: ", clf.score(x_test, y_test))
 
+    # fills in missing values with classifier predicted values
     df_nans = df.loc[~not_nans].copy()
     df_nans['score_industry'] = clf.predict(df_nans[coi])
     df.score_industry.fillna(df_nans.score_industry, inplace=True)
@@ -55,20 +70,28 @@ def score_industry_fill(df):
 
 
 def score_overall_fill(df):
+
+    # columns we will se for predicting score_overall
     coi = ['score_teaching',
            'score_research',
            'score_citation',
            'score_int_outlook',
            ]
 
+    # gets all rows that are not missing any value in any column
     not_nans = df['score_overall'].notnull()
     df_not_nans = df[not_nans]
 
+    # features
     x = np.array(df_not_nans[coi])
+    # labels
     y = np.array(df_not_nans['score_overall'])
+    # splits non empty rows into training and testing data
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 
+    # classifier that we use, Multiple Linear Regression
     clf = LinearRegression()
+    # 
     clf.fit(x_train, y_train)
     print("score_overall: ", clf.score(x_test, y_test))
     df_nans = df.loc[~not_nans].copy()
